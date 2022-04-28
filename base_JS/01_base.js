89842c19e82dd92bba2790bedcce841a7ff89242
ekaterina <zaitsevae1609@gmail.com>
2022-04-28 17:37:40 +0700

    Base

--- base_JS/01_base.js
+++ base_JS/01_base.js
@@ -1,152 +1,120 @@
 // 1 Переменные
-// camelCase
-// const firstName = 'Ekaterina'
-// //const age = 32 // number
-// const jsProgrammer = true //boolean
-//
-// const _private = 'privat'
-// const $ = 'some value'
-// const withNum5 = '5'
+// (variable - var) - создавая перементтые через var, мы можем их м
+
+// const firstName  = 'Katya'
+// const age = 32
+// const lastName = 'Zaitseva' // string
+// const  мы изменить не можем, как var
+// let age = 32 //number
+// let  - таким образом созданная переменная может изменяться также как и var
+// name = 'Katerina'
+
+ // Предпочтительно использовать const
+//  const isProgrammer = true // boolean
+// console.log(isProgrammer)
 
 // 2 Мутирование
-//console.log('Persons name: ' + firstName + ', and persons age: ' + age)
-//alert('Persons name: ' + firstName + ', and persons age: ' + age)
+// console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age) //конкатенирование (склеивание объектов линейной струк-ры, оычно строк)
+// alert('Имя человека: ' + firstName + ', а возраст человека: ' + age)
 
-//const lastName = prompt('Enter last name')
-//alert(firstName + ' ' + lastName)
+// const lastName = prompt ('Введите фамилию') //возвращает значение
+// alert(firstName + ' ' + lastName)
 
 // 3 Операторы
+// let currentYear = 2022
+// const birthYear = 1989
 
+// const age = currentYear - birthYear
 
-//const age = currentYear - birthYear
-
-
-//c += a
-//c -= a
-//c *= a
-//c /= a
-
-
+// const a = 10
+// const b = 5
 // console.log(a + b)
-// console.log(a - b)
-// console.log(a * b)
-// console.log(a / b)
-// console.log(++currentYear)
-// console.log(--currentYear)
+//
+// let c = 32
+// c = c + a эту операцию мы можем сократить, написав
+// c += a
+// c /= b
 // console.log(c)
 
-// 4 Типы данных
+// console.log(currentYear++) // оператор ++ инкрементирует (увеличивает значение переменной)
+// console.log(++currentYear)
+// console.log(currentYear--) //--деинкрементирует
+// console.log(--currentYear)
 
+// 4 Типы данных (примитивы в языке)
 // const isProgrammer = true
-// const name = 'Ekaterina'
+// const name = 'Katya'
 // const age = 32
-//  let x
-//  null
+// let x
+//
 // console.log(typeof isProgrammer)
 // console.log(typeof name)
 // console.log(typeof age)
-//  console.log(typeof x)
-//  console.log(typeof null)
+// console.log(typeof x)
+// console.log(null)
 
-// 5 Приоритет оператора
-//  const fullAge = 32
-//  const birthYear = 1989
-//  const currentYear = 2022
+// 5 Приоритет операторов
+// const fullAge = 32
+// const birthYear = 1989
+// const currentYear = 2022
 //
-//  const isFullAge = currentYear - birthYear >= fullAge //32 >= 33 => true
-//  console.log(isFullAge)
+// // > < >= <=
+// const isFullAge = (currentYear - birthYear) >= fullAge //26 >= 27 => true
+// console.log(isFullAge)
 
- // 6 Условные операторы
+// 6 Условные операторы
 
-//  const courseStatus = 'fail' //ready, fail, pending
+// const courseStatus = 'fail' //ready. fail, pending
 //
-//  if (courseStatus === 'ready') {
-// console.log('Курс уже готов и его можно проходить')
-//  } else if (courseStatus === 'pending') {
-//      console.log('Курс находиться в процессе разработки')
-//  } else {
-//      console.log('Курс не получился')
-//  }
+// if (courseStatus === 'ready') { //=== лучше использовать такое равно, исключает возможность ошибки
+//     console.log('Курс готов')
+// } else if (courseStatus === 'pending') {
+//     console.log('Курс находится в разработке')
+// } else {
+//     console.log('Курс стартует в следующем году')
+// }
+
+const isReady = true
+
+// if (isReady) {
+//     console.log('Все готово')
+// }else {
+//     console.log('Все не готово')
+// }
+
+// Тернарное выражение - используется для простых условий
+// isReady ? console.log('Все готово') :console.log('Все не готово')
+
+
+// const num1 = 42 //number
+// const num2 = '42' //string
 //
-//  const isReady = false
+// console.log(num1 === num2)
 
- // if (isReady) {
- //     console.log('Все готово!')
- // } else {
- //     console.log ('Все не готово')
- // }
-// //Тернарное выражение
-//  isReady ? console.log('Все готово!') : console.log('Все не готово')
+// 7. Булевая логика
 
-//  const num1 = 42 //number
-//  const num2 = '42' //string
-// console.log (num1 === num2 )
-
- // 7 Булевая логика
-
- // 8 Функции
- // function  calculateAge(year) {
- //    return 2022 - year
-
-
- // console.log(calculateAge(year: 1989))
- // console.log(calculateAge(year: 1993))
- // console.log(calculateAge(year: 1999))
-
-//  function logInfoAbout(name, year) {
-//      const age = calculateAge(year)
-//
-//      if (age > 0) {
-//          console.log('Человек по имени ' + name + ' сейчас имеет возраст ' + age)
-//      } else {
-//          console.log('Вообще-то это уже будущее')
-//      }
-//  }
-//
-// logInfoAbout("Ekaterina", '1989')
-//  logInfoAbout("Andrey", '1984')
-//  logInfoAbout("Maria", '2023')
-
- //9 Массивы
-//  const cars = ['BMW', 'Nisan', 'Lexus']
-// // const cars = new Array('BMW', 'Nisan', 'Lexus')
-//  console.log(cars.length)
-//
-//  console.log(cars[1])
-//  console.log(cars[0])
-//  console.log(cars[2])
-//
-//  cars[1] = 'Porsche'
-//  cars[cars.length] = 'Mazda'
-//  console.log(cars)
-
- // 10 Циклы
- // const cars = ['BMW', 'Nisan', 'Lexus', 'Porsche']
-
- // for (let i = 0; i < cars.length; i++) {
- //     const car = cars[i]
- //     console.log(car)
- // }
-
- // for (let car of cars) {
- //     console.log(car)
- // }
-
-// 11 Объкты
-const person = {
-    firstName: 'Ekaterina',
-    lastName: 'Zaitseva',
-    year: 1989,
-    Languages: ['Ru', 'En'],
-    hasHusband: true,
-    greet: function () {
-        console.log('greet for person')
-    }
+// 8. Функции
+function calculateAge (year) {
+    return 2022 - year
 }
 
-console.log(person.firstName)
-console.log(person['lastName'])
-const key = 'year'
- console.log(person[key])
- person.greet()
+// console.log(calculateAge (1989))
+// console.log(calculateAge (1984))
+// console.log(calculateAge (1969))
+// console.log(calculateAge (2006))
+// console.log(calculateAge (1925))
+// console.log(calculateAge (1917))
 
+// function  logInfoAbout (name, year) {
+//     const age = calculateAge(year)
+//
+//     if (age > 0) {
+//         console.log('Человек по имени ' + name + ' сейчас имеет возраст ' + age)
+//     } else {
+//         console.log('Вообще-то это уже будущее')
+//     }
+// }
+// logInfoAbout('Katya', 1989)
+// logInfoAbout('Katya', 2025)
+
+// 9. Массивы
